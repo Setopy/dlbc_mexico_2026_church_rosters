2e0e25e83823aba055c4a4998ce038f862c4824e
Setopy <137697494+Setopy@users.noreply.github.com>
2026-01-03 18:44:05 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dlbc-roster-v4';
+const CACHE_NAME = 'dlbc-roster-v5';
 const urlsToCache = ['./', './index.html', './app.js', './schedules.js', './manifest.json', './pastor.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
